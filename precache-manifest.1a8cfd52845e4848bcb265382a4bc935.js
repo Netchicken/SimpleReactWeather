5d6f8c0b5c79c1a71709adb2f205de8b0a4a7d2e
Gary Dix <intelproof@gmail.com>
2020-01-20 13:24:16 +1300

    Updates

---
+++ precache-manifest.1a8cfd52845e4848bcb265382a4bc935.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "279d102ea5994a8da94482fa430ddc20",
+    "url": "/SimpleReactWeather/index.html"
+  },
+  {
+    "revision": "e1f24b80824b202d44b4",
+    "url": "/SimpleReactWeather/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9a6fe5a65f41ae729364",
+    "url": "/SimpleReactWeather/static/css/main.7b9f72f2.chunk.css"
+  },
+  {
+    "revision": "e1f24b80824b202d44b4",
+    "url": "/SimpleReactWeather/static/js/2.24a9a711.chunk.js"
+  },
+  {
+    "revision": "a4a3855e93dcac654affa0bd56a1c607",
+    "url": "/SimpleReactWeather/static/js/2.24a9a711.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a6fe5a65f41ae729364",
+    "url": "/SimpleReactWeather/static/js/main.f5ead026.chunk.js"
+  },
+  {
+    "revision": "4a70452c5ee86bb81e3d",
+    "url": "/SimpleReactWeather/static/js/runtime-main.746cc517.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/SimpleReactWeather/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/SimpleReactWeather/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
